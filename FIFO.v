1d4475b4a883497992ce812aa883ceeb8dd2988c
Halit KOŞDU <112547089+halitksdu@users.noreply.github.com>
2024-07-03 14:12:43 +0300

    Update FIFO.v

--- FIFO.v
+++ FIFO.v
@@ -11,32 +11,33 @@ module FIFO(
     reg activate;
     reg [29:0] sayac;
     always @(posedge clk) begin
-		if(rd_en == 0) begin
-		      sayac <= sayac + 1;
-		      if (sayac == 3000000) // *3000000 yap
-		      	activate <= 1;
-		end else begin
-			  sayac <= 0;
-			  activate <= 0;
-	    end
+	    
+	if(rd_en == 0) begin
+		sayac <= sayac + 1;
+		if (sayac == 3000000) activate <= 1;
+		
+	end else begin
+		sayac <= 0;
+		activate <= 0;
+	end
     end
     
     // Module:
     reg [$clog2(depth):0] point_wr; // depth'in binary hali
     reg [$clog2(depth):0] point_rd;
     always @(posedge clk) begin
+	    
         if(reset == 0) begin
             point_wr <= 0;
             point_rd <= 0;
             d_out <= 8'd0;
-            for(i=0;i<depth;i=i+1) begin
-                FIFO[i]<= 8'd0;
-            end 
+            for(i=0;i<depth;i=i+1) FIFO[i]<= 8'd0;
+		
         end else if(wr_en == 1 && full == 0) begin
             FIFO[point_wr] <= d_in;
             point_wr <= point_wr +1;
+		
         end else if(activate==1 && rd_en == 0) begin
-            
             d_out <= FIFO[point_rd];
             point_rd <= point_rd +1;
             if (point_rd>point_wr) point_rd <= 0;
@@ -44,15 +45,16 @@ module FIFO(
         end 
     end 
     
-    // Full ve empty 
+    // Full ve Empty: 
     always @(*) begin
-        if (point_wr == (depth))
+	if (point_wr == (depth)) begin
             full= 1'b1;
-        else if (point_wr == 0 || (point_rd>point_wr)) begin
+	end else if (point_wr == 0 || (point_rd>point_wr)) begin
             empty= 1'b1;
-						full= 1'b0;
+	    full= 1'b0;
         end else begin
             full= 1'b0;
-            empty= 1'b0; end
+            empty= 1'b0; 
+	end
     end
 endmodule
